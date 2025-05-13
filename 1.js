4e5c627f1e4c89d8c40d3e26c7a405d312417cbc
ManuRC14 <manuelarodriguezcadena14@gmail.com>
2025-05-13 11:54:19 -0500

    Cambios repositorio local a remoto

---
+++ 1.js
@@ -0,0 +1,14 @@
+//length
+let texto = "Hola Mundo";
+console.log(texto.length);
+
+let texto2 = "Hola Mundo";
+console.log (texto2.slice(0,4));
+
+let texto3 = "Hola Mundo";
+console.log (texto3.charAt(5));
+
+let texto4 = "Hola Mundo";
+let subCadena = texto4.substring(0,4);
+console.log(subCadena); 
+
